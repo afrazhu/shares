4bc029c04737b4347ace38b6161c88b79c4cdcc1
Afra <zhuyanxia@glz8.com>
2018-04-09 19:21:19 +0800

    php7 mysqli

--- getcode.php
+++ getcode.php
@@ -1,7 +1,12 @@
 <?php
 $file_handle = @fopen('table.txt','r');
-$conn = mysql_connect('127.0.0.1','root',123456) or die('connect failed'.mysql_error());
-mysql_select_db('test',$conn);
+//php7连接数据库
+$conn = mysqli_connect('127.0.0.1','root',123456,'test');
+if (mysqli_connect_errno($conn))
+{
+    echo "连接 MySQL 失败: " . mysqli_connect_error();
+}
+//mysqli_select_db($conn,'test');
 while($line = fgets($file_handle)){
 	$code = substr($line,2,6);
 	$type = strtolower(substr($line,0,2));
@@ -11,8 +16,11 @@ while($line = fgets($file_handle)){
 	}else{
 		$order = 1;
 	}
+	//echo $line.'<br/>'.$code.'|'.$type.'|'.$num;die;
 	//echo $num;
-	mysql_query("insert into code_num (code,type,ord) values ('".$code."','".$type."','".$order."')");
+    if($code){
+        mysqli_query($conn,"insert into code_num (code,code_type,ord) values ('".$code."','".$type."','".$order."')");
+    }
 	
 }
-mysql_close($conn);
+mysqli_close($conn);
